48e8e6210db23595be54bd1a77bbc7868c5a078b
wonsik <70436955+wonsik3686@users.noreply.github.com>
2025-04-08 02:14:21 +0900

    feat: ESLint 설정 파일 업데이트 및 관련 패키지 추가 #3
    - eslint.config.mjs 파일에 TypeScript 및 React 관련 ESLint 플러그인 추가
    - package.json 및 package-lock.json에 ESLint 관련 패키지 추가 및 버전 업데이트
    - ESLint 규칙 및 설정을 TypeScript 프로젝트에 맞게 조정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,85 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.next/**',
+      '**/*.config.mjs',
+      'postcss.config.mjs',
+    ],
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: reactPlugin,
+      import: importPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...typescriptEslintPlugin.configs['recommended'].rules,
+      ...typescriptEslintPlugin.configs['recommended-type-checked'].rules,
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-console': 'error',
+      'no-plusplus': 'off',
+      'no-restricted-syntax': 'off',
+      'consistent-return': 'off',
+      'no-underscore-dangle': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'import/prefer-default-export': 'off',
+      'class-methods-use-this': 'off',
+      'react/state-in-constructor': 'off',
+      'react/static-property-placement': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'jsx-a11y/anchor-is-valid': 'off',
+      'react/require-default-props': 'off',
+      'import/extensions': 'off',
+      'react/display-name': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      'no-param-reassign': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      // 'max-len': ['error', { code: 80, ignoreUrls: true }],
+      'import/no-cycle': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/no-floating-promises': 'off',
+    },
+  },
 ];
-
-export default eslintConfig;
